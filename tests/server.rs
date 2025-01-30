9a0e1f979c85a45780bf71f669da4cc91ecd3f5b
sevki <s@sevki.io>
2025-01-30 12:32:27 +0000

    fix: dependency order

--- tests/server.rs
+++ tests/server.rs
@@ -24,7 +24,9 @@ impl Echo for EchoImpl {
 
 const PORT: u16 = 1738;
 
-async fn bind_to_v4(port: u16) -> std::result::Result<TcpListener, std::io::Error> {
+async fn bind_to_v4(
+    port: u16,
+) -> std::result::Result<TcpListener, std::io::Error> {
     TcpListener::bind((IpAddr::from(Ipv4Addr::UNSPECIFIED), port)).await
 }
 
@@ -35,25 +37,25 @@ async fn bind() -> std::result::Result<TcpListener, std::io::Error> {
 fn network_partitions_during_connect() -> turmoil::Result {
     let mut sim = Builder::new().build();
 
-    sim.host("server", || {
-        async {
-            let listener = bind().await?;
-            loop {
-                let (stream, _) = listener.accept().await?;
-                let echo = EchoImpl {};
-                let servercodec: jetstream::prelude::server::service::ServerCodec<
-                    echo_protocol::EchoService<EchoImpl>,
-                > = Default::default();
-                let framed = Framed::with_capacity(stream, servercodec, 1024 * 1024 * 10);
-                let mut serv = echo_protocol::EchoService { inner: echo };
-                run(&mut serv, framed).await.expect("server run failed");
-            }
+    sim.host("server", || async {
+        let listener = bind().await?;
+        loop {
+            let (stream, _) = listener.accept().await?;
+            let echo = EchoImpl {};
+            let servercodec: jetstream::prelude::server::service::ServerCodec<
+                echo_protocol::EchoService<EchoImpl>,
+            > = Default::default();
+            let framed =
+                Framed::with_capacity(stream, servercodec, 1024 * 1024 * 10);
+            let mut serv = echo_protocol::EchoService { inner: echo };
+            run(&mut serv, framed).await.expect("server run failed");
         }
     });
 
     sim.client("client", async {
         let stream = TcpStream::connect(("server", PORT)).await?;
-        let client_codec: jetstream_client::ClientCodec<EchoChannel> = Default::default();
+        let client_codec: jetstream_client::ClientCodec<EchoChannel> =
+            Default::default();
         let mut framed = Framed::new(stream, client_codec);
         let mut chan = EchoChannel {
             inner: Box::new(&mut framed),
@@ -63,4 +65,10 @@ fn network_partitions_during_connect() -> turmoil::Result {
     });
 
     sim.run()
+}
+
+#[okstd::test]
+fn e2e() {
+    network_partitions_during_connect()
+        .expect("network partitions during connect failed");
 }
