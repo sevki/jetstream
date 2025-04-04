7412f3e0db025b8b460528d49696424c94263b4a
sevki <s@sevki.io>
2025-04-04 13:59:17 +0100

    fix: extern async_trait and trait_variant and lazy_static

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-#![doc(html_logo_url = "https://raw.githubusercontent.com/sevki/jetstream/main/logo/JetStream.png")]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/sevki/jetstream/main/logo/JetStream.png"
+)]
 #![doc(
     html_favicon_url = "https://raw.githubusercontent.com/sevki/jetstream/main/logo/JetStream.png"
 )]
@@ -7,26 +9,25 @@
 //! JetStream, is a collection of crates that provide a set of tools to build distributed systems.
 //! It started it's life off in the CrosVM project, and has since been extracted into it's own project.
 //! For more information please see the [JetStream Book](https://sevki.github.io/jetstream)
+
+extern crate async_trait;
+extern crate lazy_static;
+extern crate trait_variant;
+
 pub mod prelude {
 
     pub use jetstream_macros::{service, JetStreamWireFormat};
 
     pub use jetstream_rpc::{
-        ClientTransport,
-        Error,
-        Frame,
-        Framer,
-        Message,
-        Protocol,
-        ServiceTransport,
-        Tag,
+        ClientTransport, Error, Frame, Framer, Message, Protocol,
+        ServiceTransport, Tag,
     };
 
     pub use lazy_static::*;
 
     pub use jetstream_wireformat::{Data, WireFormat};
 
-    pub use {async_trait::async_trait, trait_variant};
+    pub use {async_trait::async_trait, trait_variant::make};
 
     #[cfg(feature = "9p")]
     pub mod p9 {
